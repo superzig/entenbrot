ba184366d93f0543df2c4907dbaef5ab05ddb2c9
Justin Jansen <jjansen@databay.de>
2024-02-15 09:16:44 +0100

    chore: added shadcn ui libary && helper methods for tailwind css

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,8 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import MaxWidthWrapper from "~/app/_components/src/components/MaxWidthWrapper";
+import { cn } from "~/lib/utils"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,9 +23,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+    <html lang="en" className="light">
+      <body className={cn(
+          'min-h-screen font-sans antialiased grainy',
+          inter.className
+      )}>
+        <TRPCReactProvider>
+          {children}
+        </TRPCReactProvider>
       </body>
     </html>
   );

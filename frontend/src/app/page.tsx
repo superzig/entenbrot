92890f15a835c6ab85caada34831fad733a694fc
ZyklopRT <nomail@example.com>
2024-02-16 15:02:07 +0100

    style: added navbar && company name

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,11 +2,10 @@ import {unstable_noStore as noStore} from "next/cache";
 
 import {CreatePost} from "~/app/_components/create-post";
 import {api} from "~/trpc/server";
-import MaxWidthWrapper from "~/app/_components/MaxWidthWrapper";
+import MaxWidthWrapper from "~/app/_components/ui/MaxWidthWrapper";
 import Link from "next/link";
 import {ArrowRight} from "lucide-react";
 import {buttonVariants} from "~/app/_components/ui/button";
-import {InputFile} from "~/app/_components/ui/fileInput";
 
 export default async function Home() {
     noStore();
@@ -20,7 +19,7 @@ export default async function Home() {
                     <div
                         className="mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
                         <p className="text-sm font-semibold text-gray-700">
-                            $Nein$ ist jetzt öffentlich!
+                            Pathway ist jetzt öffentlich!
                         </p>
                     </div>
                     <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
@@ -28,14 +27,14 @@ export default async function Home() {
                         geeigneten  <span className="text-blue-500">Veranstaltungen</span>.
                     </h1>
                     <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-                        $Nein$ ermöglicht die einfache Zuordnung von Schülern zu Veranstaltungen. Dokumente hochladen und direkt starten.
+                        Pathway ermöglicht die einfache Zuordnung von Schülern zu Veranstaltungen. Dokumente hochladen und direkt starten.
                     </p>
 
                     <Link className={buttonVariants({
                         size: 'lg',
                         className: "mt-5"
-                    })} href="/upload" target="_blank">
-                        Los geht`s <ArrowRight className="ml-2 h-5 w-5"/>
+                    })} href="/upload">
+                        Los geht&#96;s <ArrowRight className="ml-2 h-5 w-5"/>
                     </Link>
                 </div>
             </MaxWidthWrapper>

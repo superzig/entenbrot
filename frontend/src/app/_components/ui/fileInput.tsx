e5c353badc8f8dbf23210273bc9a8f48e91f35e6
Justin Jansen <jjansen@databay.de>
2024-02-23 10:47:08 +0100

    chore: added default desihn for upload with business logic

--- frontend/src/app/_components/ui/fileInput.tsx
+++ frontend/src/app/_components/ui/fileInput.tsx
@@ -1,13 +1,39 @@
+import {useState} from "react";
+import {cn} from "~/lib/utils";
+
 interface Props {
-    children: string;
+    onUpload: (file: File) => void;
 }
 
-export function InputFile({children}: Props) {
+export function InputFile({onUpload}: Props) {
+
+    const [error, setError] = useState<string | null>(null);
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files ? event.target.files[0] : null;
+
+        if (!file) {
+            setError('Es konnte keine Datei gefunden werden')
+            return;
+        }
+        setFile(file);
+        const fileName = file.name;
+
+        const fileExtension = fileName.slice(((fileName.lastIndexOf(".") - 1) >>> 0) + 2);
+
+        if (fileExtension === 'xls' || fileExtension === 'xlsx') {
+            setError(null)
+            onUpload(file);
+            return;
+        }
+
+        setError('Die hochgeladene Datei ist keine Excel-Datei.')
+    }
     return (
         <div className="flex-col w-full">
-            <h2 className="">{children}</h2>
             <label htmlFor="uploadFile1"
-                   className="p-2 bg-white text-black text-base rounded w-full h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mx-auto font-[sans-serif]">
+                   className={cn("p-2 bg-white text-black text-base rounded w-full h-52 flex flex-col items-center justify-center cursor-pointer border-2 mx-auto", (!error && file) ? 'border-green-600' : (error && file) ? 'border-red-600' : 'border-gray-300 border-dashed')}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-8 mb-2 fill-black" viewBox="0 0 32 32">
                     <path
                         d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
@@ -16,9 +42,10 @@ export function InputFile({children}: Props) {
                         d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
                         data-original="#000000"/>
                 </svg>
-                Upload file
-                <input type="file" id='uploadFile1' className="hidden"/>
-                <p className="text-xs text-gray-400 mt-2">PNG, JPG SVG, WEBP, and GIF are Allowed.</p>
+                {(file?.name) ? file.name : 'Datei hochladen' }
+                {error && <p className="text-red-600 text-xs">{error}</p>}
+                <input type="file" id='uploadFile1' className="hidden" onChange={handleFileChange}/>
+                <p className="text-xs text-gray-400 mt-2">Nur Excel-Dateien sind erlaubt.</p>
             </label>
         </div>
     )
--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -1,11 +1,39 @@
+"use client";
 import MaxWidthWrapper from "~/app/_components/ui/MaxWidthWrapper";
+import {InputFile} from "~/app/_components/ui/fileInput";
+import {useState} from "react";
 
 
-export default async function Page() {
+export default function Page() {
 
+    const [students, setStudents] = useState<[]| null>(null);
+    console.log(students)
+    const onUpload = (file: File) => {
+        setStudents(["asd"])
+    }
     return (
-        <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col">
-            123
+        <>
+            <MaxWidthWrapper className="mb-12 mt-28 flex flex-col">
+                <div className="mb-4">
+                    <div
+                        className="mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+                        <p className="text-sm font-semibold text-gray-700">
+                            Import
+                        </p>
+                    </div>
+                    <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl"><span className="text-blue-500">
+                        Schülerliste</span> {(students === null) ? 'hochladen' : 'bearbeiten'}
+                    </h1>
+                    <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+                        Bitte laden Sie die Datei mit den Informationen zur Schülern hoch. Es können bereits Wünsche
+                        vordefiniert sein.
+                    </p>
+                </div>
+                <InputFile onUpload={onUpload}></InputFile>
+            </MaxWidthWrapper>
+        <MaxWidthWrapper>
+            12321
         </MaxWidthWrapper>
+        </>
     );
 }

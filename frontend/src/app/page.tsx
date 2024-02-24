bd4ee089a33ccddc0a8bceb52f60c7bc764e0388
ZyklopRT <nomail@example.com>
2024-02-24 12:37:53 +0100

    perf: optimized image loading of landpage image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,19 +2,31 @@ import MaxWidthWrapper from '~/app/_components/ui/MaxWidthWrapper';
 import Link from 'next/link';
 import { ArrowRight } from 'lucide-react';
 import { buttonVariants } from '~/app/_components/ui/button';
+import eventsImage from "../../public/landing-page-hero-event.png";
+import Image from "next/image";
 
 export default async function Home() {
   return (
     <>
-      <MaxWidthWrapper className='mb-12 mt-28 flex flex-col sm:mt-40'>
+      <MaxWidthWrapper className='mb-12 mt-10 flex flex-col sm:mt-40'>
         <div className='relative'>
-          <div className="absolute right-0 h-[600px] w-[600px] bg-[url('/landing-page-hero-event.png')] bg-contain bg-right bg-no-repeat"></div>
+          <Image
+              alt="Events Image"
+              src={eventsImage}
+              quality={100}
+              sizes="100vw"
+              style={{
+                height: "600px",
+                width: "600px",
+              }}
+              className="bg-right absolute right-0 bg-contain hidden md:block pointer-events-none"
+          />
           <div className='mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
             <p className='text-sm font-semibold text-gray-700'>
               Pathway ist jetzt öffentlich!
             </p>
           </div>
-          <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+          <h1 className='max-w-4xl text-4xl font-bold md:text-6xl lg:text-7xl'>
             Zuweisung von Schülern zu den geeigneten{' '}
             <span className='text-blue-500'>Veranstaltungen</span>.
           </h1>

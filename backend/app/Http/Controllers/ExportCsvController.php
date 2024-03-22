279e9f3c8cd6031ef1b5c52822db1ef09d5e7da6
itsandre.mov <andrefischer2109@gmail.com>
2024-03-22 11:56:32 +0100

    export csv

--- backend/app/Http/Controllers/ExportCsvController.php
+++ backend/app/Http/Controllers/ExportCsvController.php
@@ -8,6 +8,8 @@ use http\Env\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Illuminate\Support\Facades\Storage;
 use ZipArchive;
 
 class ExportCsvController extends BaseController
@@ -75,52 +77,62 @@ class ExportCsvController extends BaseController
     }
 
     /**
-     * Generates a CSV attendance list based on the provided data.
+     * Generiert eine CSV-Anwesenheitsliste basierend auf den bereitgestellten Daten und stellt sie zum Download bereit.
      *
-     * @param array $data An array of names for the attendance list.
+     * @param string $cacheKey Der Cache-Schlüssel für die Anwesenheitslisten-Daten.
      *
-     * @throws Exception If writing to the CSV file fails.
+     * @throws Exception Wenn das Schreiben in die CSV-Datei fehlschlägt.
      */
-    public function generatePresenceList(array $data): void
+    public function generatePresenceList(string $cacheKey): StreamedResponse|null
     {
-        try {
-            // Create CSV file
-            $timestamp = date('Y-m-d_H-i-s');
-            $csvFileName = "attendance_list_{$timestamp}.csv";
-            $csvFile = @fopen($csvFileName, 'w+');
-
-            // Check if opening the file was successful
-            if ($csvFile === false) {
-                throw new \Exception("Error creating the CSV file: {$csvFileName}");
-            }
-
-            // Write header
-            fputcsv($csvFile, ['Name', 'Anwesend'], ';', "\"");
-
-            // Write data
-            foreach ($data as $entry) {
-                // Write row to the CSV file, leave presence empty
-                if (!is_string($entry) || empty($entry)) {
-                    throw new Exception('Invalid format in data array.');
-                }
-
-                fputcsv($csvFile, [$entry, null], ';', "\"");
-            }
-
-            // Close the file
-            fclose($csvFile);
-
-            // Set HTTP headers for download
-            header('Content-Type: text/csv');
-            header("Content-Disposition: attachment; filename=\"$csvFileName\"");
-
-            // Output & delete the file
-            readfile($csvFileName);
-            unlink($csvFileName);
-            exit();
-        } catch (\Exception $e) {
-            echo "Error: " . $e->getMessage();
+        // $algoService = new AlgorithmService(); // Diese Zeile entfernen
+        // Überprüfe, ob Daten im Cache vorhanden sind
+        if (empty($this->algorithmService->getCachedData($cacheKey))) {
+            return;
         }
+
+        // Laden der Anwesenheitsliste aus dem Storage
+        $data = Storage::json("algorithm/{$cacheKey}/attendanceList.json");
+
+        $zip = new ZipArchive();
+        $zipFileName = 'output.zip';
+        if ($zip->open($zipFileName, ZipArchive::CREATE) !== TRUE) {
+            exit("Cannot open $zipFileName");
+        }
+
+        foreach ($data as $companyId => $companyData) {
+            $companyName = $companyData['company'];
+            $timeslots = $companyData['timeslots'];
+
+            foreach ($timeslots as $timeslot => $attendees) {
+                $csvFileName = "$companyName-$timeslot.csv";
+                $csvContent = "Last Name,First Name,Anwesend\n";
+                foreach ($attendees as $attendee) {
+                    $csvContent .= "{$attendee['lastName']};{$attendee['firstName']};;\n";
+                }
+                file_put_contents($csvFileName, $csvContent);
+                $zip->addFile($csvFileName);
+            }
+        }
+
+        $zip->close();
+
+        // Download the ZIP file
+        header("Content-type: application/zip");
+        header("Content-Disposition: attachment; filename=$zipFileName");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        readfile("$zipFileName");
+        // Clean up the generated CSV files
+        foreach ($data as $companyId => $companyData) {
+            $timeslots = $companyData['timeslots'];
+            foreach ($timeslots as $timeslot => $attendees) {
+                $csvFileName = "$companyName-$timeslot.csv";
+                unlink($csvFileName);
+            }
+        }
+        // Delete the zip file
+        unlink($zipFileName);
     }
 
     /**
@@ -129,132 +141,56 @@ class ExportCsvController extends BaseController
      * @param string $name The name for the running log entry.
      * @param array $runningData An array containing room numbers and dates for the running log entry.
      *
-     * @return string The filename of the generated CSV file.
      * @throws Exception If writing to the CSV file fails.
      */
-    public function generateRunningLog(string $name, array $runningData): string
+    public function generateRunningLog(string $cacheKey): void
     {
-        try {
-            // Validate input
-            if (empty($name) || empty($runningData)) {
-                throw new \Exception("Name and running data must be provided.");
-            }
-
-            // Create CSV file
-            $tempName = str_replace(' ', '_', $name);
-            $csvFileName = "laufzettel_{$tempName}.csv";
-            $csvFile = @fopen($csvFileName, 'w+');
-
-            // Check if opening the file was successful
-            if ($csvFile === false) {
-                throw new Exception("Error creating the CSV file: {$csvFileName}");
-            }
-
-            // Write header
-            fputcsv($csvFile, ['Raumnummer', 'Datum'], ';', "\"");
-
-            // Sort entries by date (assuming the date is in the second column)
-            usort($runningData, function($a, $b) {
-                return strtotime($a[1]) - strtotime($b[1]);
-            });
-
-            // Write sorted data
-            foreach ($runningData as $entry) {
-                // Validate each entry in the running data array
-                if (!is_array($entry) || count($entry) !== 2 || !is_numeric($entry[0]) || !strtotime($entry[1])) {
-                    throw new Exception('Invalid format in running data array.');
-                }
-
-                // Write row to the CSV file
-                fputcsv($csvFile, $entry, ';', "\"");
-            }
-
-            // Close the file
-            fclose($csvFile);
-
-            return $csvFileName;
-        } catch (\Exception $e) {
-            echo "Error: " . $e->getMessage();
+        if (empty($this->algorithmService->getCachedData($cacheKey))) {
+            return;
         }
-    }
 
-
-    /**
-     * Generates running logs for each entry in the provided array and creates a ZIP archive for download.
-     *
-     * @param array $logEntries An array containing entries with names and corresponding running data.
-     *
-     * @throws Exception If an error occurs during the generation of running logs or creating the ZIP archive.
-     */
-    public function generateRunningLogsForArray(array $logEntries): void
-    {
-        try {
-            // Validate input
-            if (empty($logEntries)) {
-                throw new \Exception("Log entries array must not be empty.");
-            }
-
-            // Create a temporary directory for storing individual running logs
-            $tempDir = 'temp_logs_' . uniqid();
-            if (!mkdir($tempDir) && !is_dir($tempDir)) {
-                throw new \Exception("Error creating temporary directory.");
-            }
-
-            // Generate running logs for each entry in the array
-            foreach ($logEntries as $entry) {
-                // Validate each entry in the log entries array
-                if (
-                    !is_array($entry) ||
-                    count($entry) !== 2 ||
-                    !is_string($entry[0]) ||
-                    !is_array($entry[1]) ||
-                    empty($entry[0]) ||
-                    empty($entry[1])
-                ) {
-                    throw new \Exception("Invalid format in log entries array.");
-                }
-
-                $name = $entry[0];
-                $runningData = $entry[1];
-
-                // Generate running log for the current entry
-                $csvFileName = $this->generateRunningLog($name, $runningData);
-
-                // Move the generated CSV file to the temporary directory
-                rename($csvFileName, "{$tempDir}/laufzettel_{$name}.csv");
-            }
-
-            // Create a ZIP archive of the generated running logs
-            $timestamp = date('Y-m-d_H-i-s');
-            $zipFileName = "running_logs_{$timestamp}.zip";
-            $zip = new ZipArchive();
-            if ($zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE) !== true) {
-                throw new \Exception("Error creating ZIP archive.");
-            }
-
-            // Add all CSV files from the temporary directory to the ZIP archive
-            $files = glob("{$tempDir}/*.csv");
-            foreach ($files as $file) {
-                $zip->addFile($file, basename($file));
-            }
-
-            $zip->close();
-
-            // Set HTTP headers for download
-            header('Content-Type: application/zip');
-            header("Content-Disposition: attachment; filename=\"$zipFileName\"");
-
-            // Output the ZIP archive
-            readfile($zipFileName);
-
-            // Delete the temporary directory and ZIP archive
-            array_map('unlink', glob("{$tempDir}/*"));
-            rmdir($tempDir);
-            unlink($zipFileName);
-            exit();
-        } catch (\Exception $e) {
-            echo "Error: " . $e->getMessage();
+        // Laden der Anwesenheitsliste aus dem Storage
+        $studentsData = Storage::json("algorithm/{$cacheKey}/studentSheet.json");
+        $zip = new ZipArchive();
+        $zipFileName = 'output.zip';
+        if ($zip->open($zipFileName, ZipArchive::CREATE) !== TRUE) {
+            exit("Cannot open $zipFileName");
         }
+
+        foreach ($studentsData as $student) {
+            $lastName = $student['lastName'];
+            $firstName = $student['firstName'];
+            $assignments = $student['assignments'];
+
+            $csvFileName = "$lastName-$firstName-assignments.csv";
+            $csvContent = "Time Slot;Room;Company;Specialization;\n";
+            foreach ($assignments as $timeSlot => $assignment) {
+                $room = $assignment['room'];
+                $company = $assignment['company'];
+                $specialization = $assignment['specialization'];
+                $eventId = $assignment['eventId'];
+                $isWish = $assignment['isWish'];
+
+                $csvContent .= "$timeSlot;$room;$company;$specialization;\n";
+            }
+
+        $zip->close();
+
+        // Download the ZIP file
+        header("Content-type: application/zip");
+        header("Content-Disposition: attachment; filename=$zipFileName");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        readfile("$zipFileName");
+        // Clean up the generated CSV files
+        foreach ($studentsData as $student) {
+            $lastName = $student['lastName'];
+            $firstName = $student['firstName'];
+            $csvFileName = "$lastName-$firstName-assignments.csv";
+            unlink($csvFileName);
+        }
+        // Delete the zip file
+        unlink($zipFileName);
     }
 
 
